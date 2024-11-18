7276f525ff1ec15bee87865dfb75d1983d889761
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-11-18 19:31:12 +0500

    add unit test for post endpoint in courseController

--- src/test/unit/com/kotlinspring/controller/CourseControllerUnitTest.kt
+++ src/test/unit/com/kotlinspring/controller/CourseControllerUnitTest.kt
@@ -1,7 +1,12 @@
 package com.kotlinspring.controller
 
+import com.kotlinspring.dto.CourseDTO
 import com.kotlinspring.service.CourseService
+import com.kotlinspring.util.courseDTO
 import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -15,4 +20,25 @@ class CourseControllerUnitTest {
 
     @MockkBean
     lateinit var courseServiceMockk : CourseService
+
+    @Test
+    fun addCourse() {
+        val courseDto = CourseDTO(null, "Build Restful APIs using SpringBoot and Kotlin", "Test user")
+
+        every {
+            courseServiceMockk.addCourse(any())
+        } returns courseDTO(id = 1)
+
+        val savedCourseDTO = webTestClient
+            .post()
+            .uri("/v1/courses")
+            .bodyValue(courseDto)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        assertThat(savedCourseDTO!!.id).isNotNull
+    }
 }
