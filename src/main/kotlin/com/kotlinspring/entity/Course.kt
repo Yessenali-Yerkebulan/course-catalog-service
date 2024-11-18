6274e44fda969f85a15a6d0c797f08d5b4958bc6
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-11-18 09:40:51 +0500

    create courseRepository & configure jpa in application.properties

--- src/main/kotlin/com/kotlinspring/entity/Course.kt
+++ src/main/kotlin/com/kotlinspring/entity/Course.kt
@@ -1,11 +1,13 @@
 package com.kotlinspring.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
+
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "courses")
 data class Course(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Int?,
     val name:String,
     val category: String,
