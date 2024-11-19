85d7946c2668b4c321152eed6ec5efd411cc5160
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-11-19 10:15:54 +0500

    add tests using @Parameterized test

--- src/test/intg/com/kotlinspring/repository/CourseRepositoryIntgTest.kt
+++ src/test/intg/com/kotlinspring/repository/CourseRepositoryIntgTest.kt
@@ -3,9 +3,13 @@ package com.kotlinspring.repository
 import com.kotlinspring.util.courseEntityList
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 import kotlin.test.Test
 
 @DataJpaTest
@@ -27,5 +31,22 @@ class CourseRepositoryIntgTest {
 
         assertThat(courses).isNotEmpty
         assertThat(courses.size).isEqualTo(2)
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesName_approach2(name: String, expectedSize: Int) {
+        val courses = courseRepository.findByNameContaining(name)
+        println("courses: $courses")
+        assertThat(courses).hasSize(expectedSize)
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(Arguments.arguments("SpringBoot", 2),
+                Arguments.arguments("Wiremock", 1))
+        }
     }
 }
